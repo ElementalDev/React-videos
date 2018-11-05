99b0808bfe47c6b5b2236f5a51d50241cd6b731d
RCollettSG <41951740+RCollettSG@users.noreply.github.com>
2018-11-05 16:39:37 +0000

    Added button styles to spans

--- components/App.js
+++ components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Controls from "./Controls";
 
 function App() {
-  return <div className="App" />;
+  return <Controls />;
 }
 
 export default App;
--- components/Controls.js
+++ components/Controls.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 
 class Controls extends Component {
   stats = {
-
-  }
+    message: ""
+  };
 
   render() {
     return (
-      
+      <div className="controls">
+        <span id="btnCont">
+          <i className="fas fa-backward fa-2x backBtn" id="backBtn" />
+        </span>
+        <span id="btnCont">
+          <i className="fas fa-play fa-2x playBtn" id="playBtn" />
+        </span>
+        <span id="btnCont">
+          <i className="fas fa-fast-forward fa-2x ffBtn" id="ffBtn" />
+        </span>
+      </div>
     );
   }
 }
+
+export default Controls;
