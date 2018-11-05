a1a907d7103bdcbb0dca442fcd31e95d59871731
RCollettSG <41951740+RCollettSG@users.noreply.github.com>
2018-11-05 16:43:31 +0000

    Added CSS for hover and active buttons

--- components/Controls.js
+++ components/Controls.js
@@ -8,13 +8,13 @@ class Controls extends Component {
   render() {
     return (
       <div className="controls">
-        <span id="btnCont">
+        <span className="btnCont">
           <i className="fas fa-backward fa-2x backBtn" id="backBtn" />
         </span>
-        <span id="btnCont">
+        <span className="btnCont">
           <i className="fas fa-play fa-2x playBtn" id="playBtn" />
         </span>
-        <span id="btnCont">
+        <span className="btnCont">
           <i className="fas fa-fast-forward fa-2x ffBtn" id="ffBtn" />
         </span>
       </div>
