2d0fd13829249e721b30c1ffa819a2f0e216b35a
RCollettSG <41951740+RCollettSG@users.noreply.github.com>
2018-11-05 16:13:07 +0000

    Added template for Controls

--- components/App.js
+++ components/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
---
+++ components/Controls.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Controls extends Component {
+  stats = {
+
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
